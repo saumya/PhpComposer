e41dedb7d7acf65692514f2e5fa122a6e5d2a8f3
saumya <2saumyaray@gmail.com>
2020-11-18 22:03:50 +0530

    wip: JS

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,11 @@
 console.log('Upload : App');
+
+// Define processing URL and form element
+const url = 'process.php';
+const form = document.querySelector('form');
+// Listen for form submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log('Submit : action button', new Date());
+})
