f98c71737c1b15ee817e3b745629304d2ed2dad7
saumya <2saumyaray@gmail.com>
2020-11-21 22:17:32 +0530

    UX updates

--- js/app.js
+++ js/app.js
@@ -1,29 +1,40 @@
 //
 console.log('App : version 0.1.0');
 
+const app = function(){
 
-// Getting the images
-const btnGet = document.getElementById('id_btn_getAll');
-btnGet.addEventListener('click', (event)=>{
-  event.preventDefault();
+  // Getting the images
+  const getDataFromServer = ()=>{
+    console.log('Get', new Date());
 
-  console.log('Get', new Date());
+    const url_photos = 'all_photos.php'
+    fetch( url_photos, {
+      method: 'GET'
+    }).then((response)=>{
+      //console.log( response );
+      response.json().then( result=>{
+        //console.log(result);
+        //const img = '<img src="'+ +'">'
+        let htmlStr = '';
+        result.forEach(element => {
+          htmlStr += '<img src="minstagram_uploads/'+ element +'" width="100%;"></img>';
+        });
+        //console.log(htmlStr);
+        const divPhotosDisplayContainer = document.getElementById('div_view');
+        divPhotosDisplayContainer.innerHTML = htmlStr;
+      })
+    });
+  }; // getDataFromServer
 
-  const url_photos = 'all_photos.php'
-  fetch( url_photos, {
-    method: 'GET'
-  }).then((response)=>{
-    //console.log( response );
-    response.json().then( result=>{
-      //console.log(result);
-      //const img = '<img src="'+ +'">'
-      let htmlStr = '';
-      result.forEach(element => {
-        htmlStr += '<img src="minstagram_uploads/'+ element +'" width="100%;"></img>';
-      });
-      //console.log(htmlStr);
-      const divPhotosDisplayContainer = document.getElementById('div_view');
-      divPhotosDisplayContainer.innerHTML = htmlStr;
-    })
-  });
-});// Btn Click
+  const btnGet = document.getElementById('id_btn_getAll');
+  btnGet.addEventListener('click', (event)=>{
+    event.preventDefault();
+    getDataFromServer();
+  });// Btn Click
+  
+  // Auto load the data when the page has been loaded.
+  getDataFromServer()
+
+};// app
+//
+app();
