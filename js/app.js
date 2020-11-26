1189d37da4335a3142e38953e5ec44ca53c8ddb4
saumya <2saumyaray@gmail.com>
2020-11-26 16:50:41 +0530

    Photo display UI is now loading a static JSON file instead of a PHP file call to be executed on the server

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ console.log('App : version 0.1.0');
   // Getting the images
   const getDataFromServer = ()=>{
     console.log('Get', new Date());
-
-    const url_photos = 'all_photos.php'
+    /*
+    //const url_photos = 'all_photos.php'
     fetch( url_photos, {
       method: 'GET'
     }).then((response)=>{
@@ -27,6 +27,28 @@ console.log('App : version 0.1.0');
         divPhotosDisplayContainer.innerHTML = htmlStr;
       })
     });
+    */
+    
+    // directly load JSON instead of PHP
+    const url_photos = 'minstagram_uploads/minstagram.json';
+    fetch( url_photos, { method: 'GET' } ).then(result=>{
+      //console.log( 'result', result );
+      result.json().then( data=>{
+        //console.log( 'data', data );
+        //render
+        let htmlStr = '';
+        data.map(item=>{
+          //console.log(item.file)
+          const filePath = 'minstagram_uploads/' + item.file;
+          //console.log(filePath);
+          htmlStr += '<img src='+ filePath +' width="100%;"></img>';
+        })
+        const divPhotosDisplayContainer = document.getElementById('div_view');
+        divPhotosDisplayContainer.innerHTML = htmlStr;
+      })
+    });
+
+
   }; // getDataFromServer
 
   const btnGet = document.getElementById('id_btn_getAll');
