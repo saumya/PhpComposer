c9bbd88c0a6adc7cbcb6bdf13572fdb735b12410
saumya <2saumyaray@gmail.com>
2020-11-28 15:43:48 +0530

    Loaded with AutoLoad

---
+++ src/classes/minstagram/Photo.php
@@ -0,0 +1,16 @@
+<?php
+    //
+    namespace minstagram;
+    
+    class Photo {
+        public function __construct(){
+            echo 'photo : Photo : Construct <br>';
+        }
+        public function __destruct(){
+            echo 'photo : Photo : Destruct <br>';
+        }
+        public function info(){
+            echo 'photo : Photo : info <br>';
+        }
+    }
+?>
--- study1.php
+++ study1.php
@@ -1,15 +1,51 @@
 <?php
     //
-    include_once('src/classes/App.class.php');
-    include_once('src/classes/minstagram/photo.class.php');
-    include_once('src/classes/minstagram/photolist.class.php');
+    // autoload
+    
+    spl_autoload_register(function($className){
+        echo 'xxxxxx-----------------------<br>';
+        
+        echo 'spl_autoload_register <br>';
+        echo 'Autoload class : '. $className . '<br>';
+
+        //include dirname( path.__FILE__ ) . '/' . str_replace( '\\', '/', $className ) . '.php';
+        
+        $filePathRef =  'src/classes/'. str_replace( '\\', '/', $className ) . '.php';
+        echo '> ' . $filePathRef . '<br>';
+
+        include($filePathRef);
+
+
+        //echo dirname( __FILE__ ) . '/' . str_replace( '\\', '/', $className ) . '.php <br>';
+        //echo '<br> |----| <br>';
+        //echo 'Autoload class : '. $className . '<br>';
+        echo 'xxxxxx--------------------xxx<br>';
+    });
+    
+    //
+    // include_once('src/classes/minstagram/photo.class.php');
+    // include_once('src/classes/minstagram/photolist.class.php');
+
+    //include_once('src/classes/Application.php');
 
     echo 'Study1 : Start <br>';
-    $app = new MyProject\Application();
-    $app->info();
+
+    //$app = new src\classes\Application();
+    //$app = new Application();
+    //$app->info();
+    
+    
+    
+    //include('src/classes/minstagram/Photo.php');
+    
     $photo = new minstagram\Photo();
+    //$photo = new Photo();
     $photo->info();
+    
     $photolist = new minstagram\PhotoList();
     $photolist->info();
+    
     echo 'Study1 : End <br>';
 ?>
+
+
