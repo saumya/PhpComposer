3adae75d7a0b763a6c927c4e0cfcb43d3f81900f
saumya <2saumyaray@gmail.com>
2020-11-29 22:07:27 +0530

    Monolog can not open the file!

--- study2.php
+++ study2.php
@@ -13,12 +13,20 @@ echo '<br>' . Monolog\Logger::class . '';
 echo '<br>' . Monolog\Handler\StreamHandler::class . '';
 echo '<br>' . Monolog\Handler\FirePHPHandler::class . '';
 echo '<br>';
+echo '<br>'.__DIR__.'/my_monolog.log'.'<br>';
 echo '<br> --- <br>';
 
+$filename = __DIR__.'/my_monolog.log';
+
 $logger = new Monolog\Logger('minstagram_logger');
+$streamHandler = new Monolog\Handler\StreamHandler( __DIR__.'/my_monolog.log', Logger::DEBUG);
+$logger->debug('Logger started.');
+
+
 //$logger->pushHandler(new Monolog\Handler\StreamHandler(__DIR__.'/my_monolog.log', Logger::DEBUG));
 //$logger->pushHandler(new Monolog\Handler\FirePHPHandler());
 //$logger->info('My logger is now ready');
+//echo Monolog\Logger::DEBUG . '<br>';
 
 //$logger->pushHandler(new Monolog\Handler\StreamHandler(__DIR__.'/my_monolog.log', Logger::DEBUG));
 //$logger->info('Adding a new user', ['username' => 'Seldaek']);
