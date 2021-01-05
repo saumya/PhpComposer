a2f307e653d3f50c3386244d0b0d84dc987a86ac
saumya <2saumyaray@gmail.com>
2021-01-05 15:49:37 +0530

    inserted title for the photo in DB

--- update_info_in_db.php
+++ update_info_in_db.php
@@ -5,32 +5,60 @@
 
 
 
-$app = (function(){
+$run_the_self_executing_application = (function(){
     $get_ui_data = function(){
         // ref: https://www.php.net/manual/en/wrappers.php.php
         $str_json = file_get_contents('php://input');
         $obj = json_decode( $str_json );
-        echo $obj->{'title'};
+        //echo $obj->{'title'};
+        return $obj->{'title'};
     };
     // Database
     $get_last_id = function(){
-        
-		// ref: https://www.php.net/manual/en/pdostatement.fetch.php
-		$PATH_TO_SQLITE_FILE = 'phpsqlite.db';
+        // ref: https://www.php.net/manual/en/pdostatement.fetch.php
+        $PATH_TO_SQLITE_FILE = 'phpsqlite.db';
         $pdo = new \PDO( "sqlite:" . $PATH_TO_SQLITE_FILE );
+        $sql = "SELECT * FROM minstagram";
+        $sth = $pdo->prepare( $sql );
+        $sth->execute();
+        //$result = $sth->fetchAll( PDO::FETCH_ASSOC );
+        $result = $sth->fetchAll();
+        $total_num_rows = count($result);
+        /*
+        for ($i = 0; $i < count($result); $i++)  {
+            $row = $result[$i];
+            //echo $row['id'] . ' | ' . $row['title'] . ' | ' . $row['photo_name'] . ' | ' . $row['photo'] .'<br/>';
+            echo ' \n ';
+            echo 'id=' . $row['id'] . ' | title=' . $row['title'] . ' | name=' . $row['photo_name'];
+            echo ' \n ';
+        }
+        echo 'TotalNumber='. count($result);
+        */
         //
-		$sql = "SELECT * FROM minstagram";
-		$result = $db->query( $sql );
-		
-		//$result->setFetchMode(PDO::FETCH_ASSOC);
-		//$data = json_encode( $result->fetchAll() );
-		
-        //return $data;
+        //echo 'get_last_id';
+        return $total_num_rows;
+    }; // $get_last_id/
+    
+    $set_title_for_the_photo_with_id = function($photo_id, $photo_title){
         
-        return 'get_last_id';
-	};
+        $data = [
+            'photo_id'=>$photo_id,
+            'photo_title'=>$photo_title
+        ];
+        $PATH_TO_SQLITE_FILE = 'phpsqlite.db';
+        $pdo = new \PDO( "sqlite:" . $PATH_TO_SQLITE_FILE );
+        //$sql = "SELECT * FROM minstagram";
+        //$sql = "INSERT INTO minstagram (title) VALUES (:stringtitle)";
+        $sql = "UPDATE minstagram SET title=:photo_title WHERE id=:photo_id";
+        $sth = $pdo->prepare( $sql );
+        $sth->execute($data);
+        return $pdo->lastInsertId();
+        
+    }; // $set_title_for_the_photo_with_id/
+    
     // Database/
     //
+    // Checks the folder in which photos get uploaded and counts the number of photos
     $create_file_name = function(){
         $path = 'minstagram_uploads/';
         $extensions = ['jpg', 'jpeg', 'png', 'gif'];
@@ -44,14 +72,25 @@ $app = (function(){
     // 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ( isset($_POST) ){
+            /*
             // Data from UI, the user typed title of the image
-            $get_ui_data();
+            echo '> From UI=' . $get_ui_data();
             
             // Get the files data from the folder & Make a new Name and save it in DB
-            echo $create_file_name();
+            echo '> New FileName=' . $create_file_name();
             //
-            // TODO: Check the DB and get the last id from the table
-            echo $get_last_id();
+            // Check the DB and get the last id from the table
+            // echo '> LastId=' . $get_last_id();
+            // We do not need to check the DB for the latest 'id'
+            // We can take that from the number of files
+            echo '> LastId=' . ($create_file_name()-1);
+            //
+            */
+
+            $photo_id = ($create_file_name()-1);
+            $photo_title = $get_ui_data();
+            $result = $set_title_for_the_photo_with_id($photo_id, $photo_title);
+            echo $result;
             //
         }else{
             echo '{ "result" : "Nothing From FrontEnd" }';
@@ -64,7 +103,7 @@ $app = (function(){
 
 // =================================================
 // Execute the function
-//$app();
+$run_the_self_executing_application();
 // =================================================
 
 //
@@ -84,41 +123,9 @@ echo '<br>';
 echo( 'Next File Name =' . $next_file_name );
 */
 
-//print_r( $GLOBALS['g_file_name'] );
 
-$get_last_id = function(){
-    // ref: https://www.php.net/manual/en/pdostatement.fetch.php
-    $PATH_TO_SQLITE_FILE = 'phpsqlite.db';
-    $pdo = new \PDO( "sqlite:" . $PATH_TO_SQLITE_FILE );
-    $sql = "SELECT * FROM minstagram";
-    $sth = $pdo->prepare( $sql );
-    $sth->execute();
-    //$result = $sth->fetchAll();
-    //$result = $sth->fetchAll( PDO::FETCH_ASSOC );
-    
-    //echo count($result);
-    //echo json_encode($result);
-    //print_r($result);
-    //
-    
-    $result = $sth->fetchAll();
-    for ($i = 0; $i < count($result); $i++)  {
-        //echo $result[$i] ."<br />";
-        /*
-        for($j = 0; $j< count($result[$i]); $j++) {
-            echo $result[$i][$j]."<br />";
-        }
-        */
-        $row = $result[$i];
-        //echo $row['id'] . ' | ' . $row['title'] . ' | ' . $row['photo_name'] . ' | ' . $row['photo'] .'<br/>';
-        echo $row['id'] . ' | title=' . $row['title'] . ' | name=' . $row['photo_name'] . '<br/>';
-    }
-    
-    //$result = $sth->fetchAll( PDO::FETCH_ASSOC );
 
-    //
-    //echo 'get_last_id';
-};
-$get_last_id();
+
+
 //
 
