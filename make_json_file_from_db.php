60e1aac633c38ebf8a858d13baf00ec046958c3a
saumya <2saumyaray@gmail.com>
2021-01-06 19:44:04 +0530

    making JSON file from DB

--- make_json_file_from_db.php
+++ make_json_file_from_db.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hello from SERVER';
+// Setting the Server Time Zone
+date_default_timezone_set('Asia/Kolkata');
+
+//$dateFormat = "Y n j, g:i:s a";
+$dateFormat = "F j, Y, g:i:s a";
+
+$d = date($dateFormat) ;
+echo $d;
+
+//echo "$d Hello from SERVER";
