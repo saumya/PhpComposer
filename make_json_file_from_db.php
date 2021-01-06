bd3c21bf66bee5743f3c78935ba7dba68a058379
saumya <2saumyaray@gmail.com>
2021-01-06 22:31:01 +0530

    done writing JSON file from DB

--- make_json_file_from_db.php
+++ make_json_file_from_db.php
@@ -34,21 +34,41 @@ date_default_timezone_set('Asia/Kolkata');
         //echo count($data);
         //print_r($data);
         //var_dump( $data );
-        
+
+        $jsonString = '';
+        $jString = '';
+
         echo '[';
         for ($i=0; $i <count($data) ; $i++) { 
             $row = $data[$i];
             //echo 'id=' . $row['id'] . '- title=' . $row['title'] . '- name=' . $row['photo_name'];
-            
             echo '{ "id" : ' . $row['id'] . ',';
             echo '"title" : "' . $row['title'] . '",';
-                if( $i === (count($data)-1) ){        
-                    echo '"name" : "' . $row['photo_name'] . '" }';
-                }else{
-                    echo '"name" : "' . $row['photo_name'] . '" },';
-                }
-        }
+            if( $i === (count($data)-1) ){
+                echo '"name" : "' . $row['photo_name'] . '" }';
+            }else{
+                echo '"name" : "' . $row['photo_name'] . '" },';
+            }// if/
+            //
+            $jString .= '{ "id" : ' . $row['id'] . ',';
+            $jString .= '"title" : "' . $row['title'] . '",';
+            $jString .= '"path" : "' . $row['id'] . '.jpg",';
+            //$jString .= '"name" : "' . $row['photo_name'] . '" }';
+            if( $i === (count($data)-1) ){
+                $jString .= '"name" : "' . $row['photo_name'] . '" }';
+            }else{
+                $jString .= '"name" : "' . $row['photo_name'] . '" },';
+            }
+            //
+        }// for/
         echo ']';
+
+        $jsonString = '[' . $jString . ']' ;
+        // Writing to JSON File
+        $fileToWrite = "minstagram_uploads/minstagram_db.json";
+        $file_open_handle = fopen( $fileToWrite, 'w' );
+        fwrite( $file_open_handle, $jsonString );
+        fclose( $file_open_handle );
         
         //
         //echo $data;
